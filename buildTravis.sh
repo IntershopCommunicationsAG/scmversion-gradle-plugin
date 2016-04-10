ae2b384e329ecacde2c34840c87c75c3db7f4743
mraab <mraab@intershop.de>
2016-04-10 20:30:14 +0200

    reduce log output

--- buildTravis.sh
+++ buildTravis.sh
@@ -4,17 +4,17 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$TRAVIS_TAG" == "" ]; then
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-        ./gradlew test build -i -s
+        ./gradlew test build -s
     else
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH'] and without Tag'
-        ./gradlew test build publishPlugins -i -s
+        ./gradlew test build publishPlugins -s
     fi
 else
     if [ "$TRAVIS_TAG" == "" ]; then
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-        ./gradlew test build -i -s
+        ./gradlew test build -s
     else
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH'] and without Tag'
-        ./gradlew test build publishPlugins -i -s
+        ./gradlew test build publishPlugins -s
     fi
 fi
