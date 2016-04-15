70068af86c34a560a1bb3c892800830d79f3aca3
mraab <mraab@intershop.de>
2016-04-15 15:19:15 +0200

    change build configuration

--- buildTravis.sh
+++ buildTravis.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This script will build the project.
 
+export JAVA_OPTS="-Xmx1024M -XX:MaxPermSize=512M -XX:ReservedCodeCacheSize=512M"
+export GRADLE_OPTS="-Dorg.gradle.daemon=true"
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$TRAVIS_TAG" == "" ]; then
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
