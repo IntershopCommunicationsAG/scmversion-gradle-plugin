586c2c0175e63e54783141fe10128eff30565077
mraab <mraab@intershop.de>
2016-06-06 22:10:22 +0200

    Change behavior of getPreviousVersion: If no previous version available the return value is an empty string. Add positiv test for previous version.

--- src/test/groovy/com/intershop/gradle/scm/IntVersionServiceSpec.groovy
+++ src/test/groovy/com/intershop/gradle/scm/IntVersionServiceSpec.groovy
@@ -25,7 +25,7 @@ class IntVersionServiceSpec extends AbstractTaskSpec {
     @Requires({ System.properties['svnurl'] &&
             System.properties['svnuser'] &&
             System.properties['svnpasswd'] })
-    def 'test previousVersion- #gradleVersion'(gradleVersion) {
+    def 'test previousVersion without previous version - #gradleVersion'(gradleVersion) {
         given:
         svnCheckOut(testProjectDir, "${System.properties['svnurl']}/trunk")
 
@@ -56,6 +56,45 @@ class IntVersionServiceSpec extends AbstractTaskSpec {
         result.task(":showVersion").outcome == SUCCESS
         result.output.contains('Project version: 1.0.0-LOCAL')
         result.output.contains('previous version: --')
+
+        where:
+        gradleVersion << supportedGradleVersions
+    }
+
+    @Requires({ System.properties['svnurl'] &&
+            System.properties['svnuser'] &&
+            System.properties['svnpasswd'] })
+    def 'test previousVersion with previous version - #gradleVersion'(gradleVersion) {
+        given:
+        svnCheckOut(testProjectDir, "${System.properties['svnurl']}/tags/SBRELEASE_2.0.0")
+
+        buildFile << """
+        plugins {
+            id 'com.intershop.gradle.scmversion'
+        }
+
+        scm {
+            prefixes {
+                tagPrefix = 'SBRELEASE'
+            }
+        }
+
+        version = scm.version.version
+
+        println "previous version: \${scm.version.previousVersion}"
+
+        """.stripIndent()
+
+        when:
+        def result = getPreparedGradleRunner()
+                .withArguments('showVersion', '--stacktrace', '-d', '-PrunOnCI=true', "-PscmUserName=${System.properties['svnuser']}", "-PscmUserPasswd=${System.properties['svnpasswd']}")
+                .withGradleVersion(gradleVersion)
+                .build()
+
+        then:
+        result.task(":showVersion").outcome == SUCCESS
+        result.output.contains('Project version: 2.0.0')
+        result.output.contains('previous version: 1.1.0')
 
         where:
         gradleVersion << supportedGradleVersions
