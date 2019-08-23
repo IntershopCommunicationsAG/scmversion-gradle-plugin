bbcbba781f942d8a208439733c0862ed398402fb
Matthias Raab <m.raab@intershop.de>
2019-08-23 10:53:11 +0200

    adapt log output

--- src/main/groovy/com/intershop/gradle/scm/version/ReleaseFilter.groovy
+++ src/main/groovy/com/intershop/gradle/scm/version/ReleaseFilter.groovy
@@ -31,7 +31,7 @@ class ReleaseFilter extends AbstractBranchFilter {
     private def regexPattern
 
     ReleaseFilter(PrefixConfig prefixConfig, Version sourceVersion) {
-        String patternString = "^${prefixConfig.getPrefix(BranchType.tag)}${prefixConfig.getTagPrefixSeperator().replace("/", "\\/") ?: prefixConfig.getPrefixSeperator().replace("/", "\\/")}"
+        String patternString = "^${prefixConfig.getPrefix(BranchType.tag)}${prefixConfig.getTagPrefixSeperator() ? prefixConfig.getTagPrefixSeperator().replace("/", "\\/") : prefixConfig.getPrefixSeperator().replace("/", "\\/")}"
         if(sourceVersion.getNormalVersion().getVersionType() == VersionType.threeDigits) {
             patternString += threeDigitsFilter
         } else {
@@ -43,7 +43,7 @@ class ReleaseFilter extends AbstractBranchFilter {
         }
         patternString += "(${Version.METADATA_SEPARATOR}(\\w+\\.?\\d+))?"
 
-        log.debug('Branch filter is {}', patternString)
+        log.debug('Release filter is {}', patternString)
         regexPattern = /${patternString}/
     }
 
