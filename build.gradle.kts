fdca9bdc36b34b0c2930d5005233a1c5835f6c7c
Matthias Raab <M.Raab@intershop.de>
2023-06-22 17:45:56 +0200

    build: move build to github und mavencentral

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,16 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import com.jfrog.bintray.gradle.BintrayExtension
 import org.asciidoctor.gradle.jvm.AsciidoctorTask
-import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.util.*
 
 plugins {
+
     // project plugins
     `java-gradle-plugin`
     groovy
+
     kotlin("jvm") version "1.3.72"
 
     // test coverage
@@ -34,26 +33,29 @@ plugins {
     // publish plugin
     `maven-publish`
 
+    // artifact signing - necessary on Maven Central
+    signing
+
     // plugin for documentation
-    id("org.asciidoctor.jvm.convert") version "3.2.0"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
 
     // documentation
-    id("org.jetbrains.dokka") version "0.10.1"
+    id("org.jetbrains.dokka") version "1.5.0"
 
     // code analysis for kotlin
-    id("io.gitlab.arturbosch.detekt") version "1.11.0"
+    id("io.gitlab.arturbosch.detekt") version "1.18.0"
 
     // plugin for publishing to Gradle Portal
-    id("com.gradle.plugin-publish") version "0.12.0"
-
-    // plugin for publishing to jcenter
-    id("com.jfrog.bintray") version "1.8.5"
+    id("com.gradle.plugin-publish") version "1.0.0"
 }
 
 // release configuration
 group = "com.intershop.gradle.scm"
 description = "Gradle SCM version plugin - SCM based version handling for Gradle"
-version = "6.2.0"
+version = "6.2.1"
+
+val sonatypeUsername: String? by project
+val sonatypePassword: String? by project
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -184,20 +186,14 @@ tasks {
         jacocoTestReport.dependsOn("test")
     }
 
-    getByName("bintrayUpload")?.dependsOn("asciidoctor")
-    getByName("publishToMavenLocal")?.dependsOn("asciidoctor")
+    getByName("jar").dependsOn("asciidoctor")
 
     val compileKotlin by getting(KotlinCompile::class) {
         kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
-    val dokka by existing(DokkaTask::class) {
-        outputFormat = "javadoc"
-        outputDirectory = "$buildDir/javadoc"
-
-        // Java 8 is only version supported both by Oracle/OpenJDK and Dokka itself
-        // https://github.com/Kotlin/dokka/issues/294
-        enabled = JavaVersion.current().isJava8
+    dokkaJavadoc.configure {
+        outputDirectory.set(buildDir.resolve("dokka"))
     }
 
     val sourcesJar = task<Jar>("sourceJar") {
@@ -207,10 +203,21 @@ tasks {
         archiveClassifier.set("sources")
     }
 
-    val javadocJar = task<Jar>("javadocJar") {
-        dependsOn(dokka)
-        from(dokka)
-        archiveClassifier.set("javadoc")
+    withType<Sign> {
+        val sign = this
+        withType<PublishToMavenLocal> {
+            this.dependsOn(sign)
+        }
+        withType<PublishToMavenRepository> {
+            this.dependsOn(sign)
+        }
+    }
+
+    afterEvaluate {
+        getByName<Jar>("javadocJar") {
+            dependsOn(dokkaJavadoc)
+            from(dokkaJavadoc)
+        }
     }
 }
 
@@ -219,8 +226,6 @@ publishing {
         create("intershopMvn", MavenPublication::class.java) {
 
             from(components["java"])
-            artifact(tasks.getByName("sourceJar"))
-            artifact(tasks.getByName("javadocJar"))
 
             artifact(File(buildDir, "docs/asciidoc/html5/README.html")) {
                 classifier = "reference"
@@ -230,61 +235,55 @@ publishing {
                 classifier = "docbook"
             }
 
-            pom.withXml {
-                val root = asNode()
-                root.appendNode("name", project.name)
-                root.appendNode("description", project.description)
-                root.appendNode("url", "https://github.com/IntershopCommunicationsAG/${project.name}")
-
-                val scm = root.appendNode( "scm" )
-                scm.appendNode( "url", "https://github.com/IntershopCommunicationsAG/${project.name}")
-                scm.appendNode( "connection", "git@github.com:IntershopCommunicationsAG/${project.name}.git")
-
-                val org = root.appendNode( "organization" )
-                org.appendNode( "name", "Intershop Communications" )
-                org.appendNode( "url", "http://intershop.com" )
-
-                val license = root.appendNode( "licenses" ).appendNode( "license" )
-                license.appendNode( "name", "Apache License, Version 2.0" )
-                license.appendNode( "url", "http://www.apache.org/licenses/LICENSE-2.0" )
-                license.appendNode( "distribution", "repo" )
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+                url.set("https://github.com/IntershopCommunicationsAG/${project.name}")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                organization {
+                    name.set("Intershop Communications AG")
+                    url.set("http://intershop.com")
+                }
+                developers {
+                    developer {
+                        id.set("m-raab")
+                        name.set("M. Raab")
+                        email.set("mraab@intershop.de")
+                    }
+                }
+                scm {
+                    connection.set("git@github.com:IntershopCommunicationsAG/${project.name}.git")
+                    developerConnection.set("git@github.com:IntershopCommunicationsAG/${project.name}.git")
+                    url.set("https://github.com/IntershopCommunicationsAG/${project.name}")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials {
+                username = sonatypeUsername
+                password = sonatypePassword
             }
         }
     }
 }
 
-bintray {
-    user = System.getenv("BINTRAY_USER")
-    key = System.getenv("BINTRAY_KEY")
-
-    setPublications("intershopMvn")
-
-    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
-        repo = "maven"
-        name = project.name
-        userOrg = "intershopcommunicationsag"
-
-        setLicenses("Apache-2.0")
-        vcsUrl = "https://github.com/IntershopCommunicationsAG/${project.name}"
-
-        desc = project.description
-        websiteUrl = "https://github.com/IntershopCommunicationsAG/${project.name}"
-        issueTrackerUrl = "https://github.com/IntershopCommunicationsAG/${project.name}/issues"
-
-        setLabels("intershop", "gradle", "plugin", "version", "release")
-        publicDownloadNumbers = true
-
-        version(delegateClosureOf<BintrayExtension.VersionConfig> {
-            name = project.version.toString()
-            desc = "${project.description} ${project.version}"
-            released  = Date().toString()
-            vcsTag = project.version.toString()
-        })
-    })
+signing {
+    sign(publishing.publications["intershopMvn"])
 }
 
 dependencies {
-    implementation("com.intershop.gradle.version:extended-version:3.0.3")
+    implementation("com.intershop.gradle.version:extended-version:3.1.0")
     implementation(gradleKotlinDsl())
     implementation(kotlin("stdlib-jdk8"))
 
@@ -297,13 +296,14 @@ dependencies {
     testRuntimeOnly("org.apache.httpcomponents:httpclient:4.5.6")
     testRuntimeOnly("org.slf4j:slf4j-api:1.7.25")
 
-    testImplementation("com.intershop.gradle.test:test-gradle-plugin:3.1.0-dev.2")
+    testImplementation("com.intershop.gradle.test:test-gradle-plugin:4.1.2")
     testImplementation(gradleTestKit())
 
     testImplementation("commons-io:commons-io:2.2")
 }
 
 repositories {
-    jcenter()
+    gradlePluginPortal()
+    mavenCentral()
 }
 
