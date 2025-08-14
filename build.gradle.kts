a37ebb4fb43a991786a28a6159442b322e85fd1b
Sven Kiesewetter <skiesewetter@intershop.com>
2025-08-14 07:42:28 +0200

    ci: migrate publishing to maven central (#106686)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 import org.asciidoctor.gradle.jvm.AsciidoctorTask
+import io.gitee.pkmer.enums.PublishingType
 
 plugins {
 
@@ -39,6 +40,8 @@ plugins {
 
     // plugin for publishing to Gradle Portal
     id("com.gradle.plugin-publish") version "1.2.1"
+
+    id("io.gitee.pkmer.pkmerboot-central-publisher") version "1.1.1"
 }
 
 // release configuration
@@ -199,6 +202,8 @@ tasks {
     }
 }
 
+val stagingRepoDir = project.layout.buildDirectory.dir("stagingRepo")
+
 publishing {
     publications {
         create("intershopMvn", MavenPublication::class.java) {
@@ -212,7 +217,8 @@ publishing {
             artifact(project.layout.buildDirectory.file("docs/asciidoc/docbook/README.xml")) {
                 classifier = "docbook"
             }
-
+        }
+        withType<MavenPublication>().configureEach {
             pom {
                 name.set(project.name)
                 description.set(project.description)
@@ -245,14 +251,26 @@ publishing {
     }
     repositories {
         maven {
-            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
-            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
-            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-            credentials {
-                username = sonatypeUsername
-                password = sonatypePassword
-            }
+            name = "LOCAL"
+            url = stagingRepoDir.get().asFile.toURI()
         }
+    }
+}
+
+pkmerBoot {
+    sonatypeMavenCentral{
+        // the same with publishing.repositories.maven.url in the configuration.
+        stagingRepository = stagingRepoDir
+
+        /**
+         * get username and password from
+         * <a href="https://central.sonatype.com/account"> central sonatype account</a>
+         */
+        username = sonatypeUsername
+        password = sonatypePassword
+
+        // Optional the publishingType default value is PublishingType.AUTOMATIC
+        publishingType = PublishingType.USER_MANAGED
     }
 }
 
